4c3357d1e0e9acbc23a7214c15a2bb7c5bfad430
guoqing <guoqing@geetest.com>
2021-12-02 15:41:10 +0800

    添加dart  UI配置--2

---
+++ lib/ol_rect.dart
@@ -0,0 +1,10 @@
+part of gt_onelogin_flutter_plugin;
+
+class OLRect{
+  double? width;
+  double? height;
+  double? x;
+  double? y;
+
+  OLRect({this.height,this.width,this.x,this.y});
+}
---
+++ lib/ol_ui_configure.dart
@@ -0,0 +1,102 @@
+part of gt_onelogin_flutter_plugin;
+
+class OLUIConfigure{
+
+  //
+  //
+  // //Only for iOS 授权页面支持的横竖屏方向
+  // OLIOSinterfaceOrientation? supported//interfaceOrientations;
+  //
+  // OLIOSUser//interfaceStyle? user//interfaceStyle;
+  //
+  // ///--------------弹窗----------------
+  // //是否是弹框样式
+  // bool? isDialogStyle;
+  // //弹窗size，位置
+  // OLRect? dialogRect;
+  // //弹窗圆角
+  // double? dialogCornersRadius;
+  //
+  // ///--------------授权页基本UI----------------
+  // //设置背景图片
+  // // String? authViewBackgroundImage;
+  // //Only for iOS 设置背景颜色
+  // //Color? backgroundColor;
+  //
+  // ///--------------状态栏----------------
+  // //Only for Android 状态栏颜色
+  // //Color? statusBarBgColor;
+  // //状态栏样式
+  // OLStatusBarStyle? statusBarStyle;
+  //
+  // ///--------------标题栏，虚拟按键----------------
+  // //标题栏：颜色
+  // //Color? navigationBarColor;
+  // //Only for Android 标题栏：高度
+  // double? authNavHeight;
+  // //标题栏：隐藏
+  // bool? navHidden;
+  // //标题栏：文本
+  // // String? navText;
+  // //标题栏：字体颜色
+  // //Color?  navTextColor;
+  // //标题栏：字体大小
+  // //int?    navTextSize;
+  // //返回按钮图片
+  // // String? navBackImage;
+  // //返回按钮图片 size 位置
+  // OLRect? navBackImageRect;
+  // //返回按钮是否隐藏
+  // bool? navBackImageHidden;
+  //
+  // ///--------------logo----------------
+  // //logo 图片
+  // // String? logoImage;
+  // //logo size 位置
+  // OLRect? logoImageRect;
+  // //logo：是否隐藏
+  // bool? logoImageHidden;
+  // //Only for iOS logo圆角，默认为0
+  // double? logoCornerRadius;
+  //
+  // ///--------------号码----------------
+  // //号码栏字体颜色
+  // //Color? numberColor;
+  // //号码栏字体大小
+  // double? numberSize;
+  // //号码栏 size 位置
+  // OLRect? numberRect;
+  //
+  // ///--------------切换账号按钮----------------
+  // // 切换账号按钮文本
+  // // String? switchButtonText;
+  // // 切换账号按钮字体颜色
+  // //Color?  switchButtonColor;
+  // // 切换账号按钮字体大小
+  // //int?  switchTextSize;
+  // // 切换账号按钮是否隐藏
+  // bool? switchButtonHidden;
+  // //Only for iOS  切换账号按钮背景颜色
+  // //Color? switchButtonBackgroundColor;
+  // // 切换账号按钮size  位置
+  // OLRect? switchButtonRect;
+  // //Only for Android 切换账号相对于底部 y 偏移
+  // double? switchButtonOffsetYB;
+  // //Only for Android 切换账号背景图片
+  // // String? switchButtonBgImage;
+  //
+  // ///--------------一键登录按钮----------------
+  // //[正常状态的背景图片, 不可用状态的背景图片, 高亮状态的背景图片],iOS数组最多为3，Android最多为2
+  // // List<String>? authButtonImages;
+  // //授权按钮的size 位置
+  // OLRect? authButtonRect;
+  // double?
+  //
+  // ///--------------隐私条款----------------
+  // //隐私条款页面标题栏文字
+  // // String? navWebViewText;
+  // //隐私条款页面标题栏字体颜色
+  // //Color?  navWebViewTextColor;
+  // //隐私条款页面标题栏字体大小
+  // //int?    navWebViewTextSize;
+}
