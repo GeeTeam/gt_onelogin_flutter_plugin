28fe8f82095dc5818e15d8df1bb436f54c054137
hexunyou <hexunyou@geetest.com>
2022-08-23 18:23:44 +0800

    修复弹窗Activity场景下息屏再亮屏后弹窗的背景黑屏的问题

--- example/android/app/src/main/kotlin/com/geetest/gt_onelogin_flutter_plugin_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/geetest/gt_onelogin_flutter_plugin_example/MainActivity.kt
@@ -3,4 +3,10 @@ package com.geetest.gt_onelogin_flutter_plugin_example
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    override fun onStart() {
+        super.onStart()
+        // 解决弹窗Activity场景息屏亮屏后弹窗的背景黑屏的问题
+        flutterEngine?.lifecycleChannel?.appIsResumed()
+    }
 }
